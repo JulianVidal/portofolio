e561af6ad645a93a2155ac2685d2f8b7c063aba2
spiderpiggy03 <49546430+spiderpiggy03@users.noreply.github.com>
2020-03-23 17:46:05 +0300

    Maze Window Size

--- CondingChallenges/MazeGenerator/js/main.js
+++ CondingChallenges/MazeGenerator/js/main.js
@@ -1,9 +1,9 @@
 import { Maze } from "../algorithms/Maze.js";
 
-window.maze = new Maze(30, 30)
+window.maze = new Maze(1000 / 10, 800 / 10)
 
-// window.maze.animateGenerate()
+window.maze.animateGenerate()
 
-window.maze.maze = window.maze.generate()
+// window.maze.maze = window.maze.generate()
 
 window.maze.draw()
